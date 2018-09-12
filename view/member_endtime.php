341e62edb8e37e24d28e6efec26f8c7ac00aa0b2
Satbirajah <38457013+Satbirajah@users.noreply.github.com>
2018-09-12 17:00:28 +0200

    Added files via upload
    Memberview

---
+++ view/member_endtime.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vmadmin
+ * Date: 12.09.2018
+ * Time: 16:43
+ */
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Arbeitsende</title>
+</head>
+<body>
+<p>Ihre Arbgeitszeit ist beendet..</p>
+</body>
+</html>
---
+++ view/member_starttime.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vmadmin
+ * Date: 12.09.2018
+ * Time: 16:41
+ */
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Arbeitsanfang</title>
+</head>
+<body>
+        <p>Ihre Arbgeitszeit hat gerade begonnen.</p>
+</body>
+</html>
