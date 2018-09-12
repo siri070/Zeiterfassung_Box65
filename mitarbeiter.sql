8c58434d2977feb681287e06d83785e98375728e
unknown <iris.bu@hotmail.com>
2018-09-12 15:33:04 +0200

    Datenbank

---
+++ mitarbeiter.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 12. Sep 2018 um 15:31
+-- Server-Version: 10.1.34-MariaDB
+-- PHP-Version: 5.6.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `zeiterfassung`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `mitarbeiter`
+--
+
+DROP TABLE IF EXISTS `mitarbeiter`;
+CREATE TABLE IF NOT EXISTS `mitarbeiter` (
+  `mid` int(11) NOT NULL AUTO_INCREMENT,
+  `benutzername` varchar(4) NOT NULL,
+  `passwort` varchar(30) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `vorname` varchar(50) NOT NULL,
+  `nachname` varchar(50) NOT NULL,
+  PRIMARY KEY (`mid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- RELATIONEN DER TABELLE `mitarbeiter`:
+--
+
+--
+-- Daten für Tabelle `mitarbeiter`
+--
+
+INSERT INTO `mitarbeiter` (`mid`, `benutzername`, `passwort`, `admin`, `vorname`, `nachname`) VALUES
+(1, 'T123', '', 0, 'Tester', 'Test'),
+(2, 'A123', '', 1, 'Admin', 'Admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
